fcc81659ff1cba8672f56b9ee2114f3f2feab453
yanqiangmiffy <1185918903@qq.com>
2018-09-19 19:02:00 +0800

    更细

--- README.md
+++ README.md
@@ -6,21 +6,24 @@ Automotive industry user opinion theme and emotion recognition
 ## svm
 
 - 情感
-
+```text
 precision:0.6858-recall:0.7136-f1:0.6665-accuracy:0.7136
-
-
+```
 - 主题
-
+```text
 precision:0.6316-recall:0.6291-f1:0.6291-accuracy:0.6291
+```
 
 ## SGDClassifier
 
 - 情感
-
+```text
 precision:0.6836-recall:0.7126-f1:0.6609-accuracy:0.7126
+```
 
 - 主题
+```text
 precision:0.6353-recall:0.6332-f1:0.6324-accuracy:0.6332 
+```
 
 
--- analysis.py
+++ analysis.py
@@ -1,17 +1,5 @@
 # 数据统计
+from snownlp import SnowNLP
 
-import pandas as pd
-import matplotlib.pyplot as plt
-# content_id,content,subject,sentiment_value,sentiment_word
-train_data=pd.read_csv('data/train.csv')
-# train_data['subject'].value_counts().plot(kind='barh')
-# plt.show()
-print(train_data['subject'].value_counts())
-print(train_data['sentiment_value'].value_counts())
-
-
-subject_labels=dict()
-for i,x in enumerate(pd.unique(train_data['subject'])):
-    subject_labels[x]=i
-train_data['subject']=train_data['subject'].apply(lambda x:subject_labels[x])
-print(train_data['subject'])
+snow=SnowNLP('哈哈，也是好车，不太喜欢它中控 ')
+print(snow.sentiments)
